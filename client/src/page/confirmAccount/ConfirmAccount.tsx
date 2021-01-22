bcde1197d1aea7891f16fb3b91e71cfd237e7a14
vasilets1998 <47006146+vasilets1998@users.noreply.github.com>
2021-01-22 12:04:34 +0200

    comit fix confirm account err, del react import

--- client/src/page/confirmAccount/ConfirmAccount.tsx
+++ client/src/page/confirmAccount/ConfirmAccount.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import {CONFIRM_ACCOUNT_URL} from "../../utils/urls";
 import {getConfirmAccountApi} from "../../request/AccountRequest";
 import {useHistory} from "react-router-dom";
