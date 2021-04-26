e3d247a0929c616075b473446d982fdfead722d3
vasilets1998 <47006146+vasilets1998@users.noreply.github.com>
2021-04-26 16:13:56 +0300

    add btn

--- bot/resorce/bot.py
+++ bot/resorce/bot.py
@@ -1,7 +1,7 @@
 from telebot import TeleBot, types
 
 from config import Config
-from resorce.botMsg import answer
+from utils.answer.answer import answer
 from utils.decorator.decorator import request_printer
 from utils.request.requests import handle_request
 
@@ -16,15 +16,16 @@ class BotHandler(object):
         markup = types.ReplyKeyboardMarkup()
         item_news = types.KeyboardButton('Новости')
         item_time_table = types.KeyboardButton('Расписане')
+        item_match_result = types.KeyboardButton('Результаты')
         item_tournament = types.KeyboardButton('Турнирная Таблица')
         item_team = types.KeyboardButton('Команды')
         item_scorers = types.KeyboardButton('Бомбардиры')
 
         markup.row(item_news)
-        markup.row(item_time_table, item_tournament)
+        markup.row(item_time_table, item_tournament, item_match_result)
         markup.row(item_team, item_scorers)
 
-        bot.send_message(message.from_user.id, answer["/start"], reply_markup=markup)
+        bot.send_message(message.from_user.id, answer[message.text]["msg"], reply_markup=markup)
 
     @staticmethod
     @bot.message_handler(content_types=['text'])
@@ -35,7 +36,8 @@ class BotHandler(object):
         if type(switcher) == str: return bot.send_message(message.from_user.id, switcher)
 
         response = handle_request(switcher["route"])
-        result = switcher["html"](response["data"], message.text)
+        result = response["error"] if response.get("error") else switcher["html"](response["data"]["data"],
+                                                                                  message.text)
 
         bot.send_message(message.from_user.id, result, parse_mode='HTML')
 
